a26d05f6e5d12d63fe15657cefb8728070b388f0
MouseHoleStudios <mconeymusic@gmail.com>
2023-07-31 21:33:22 -0400

    library update
    code switched to Arduino library format. To use, copy to arduino library directory

---
+++ SevenSegClock/src/SevenSegClock.h
@@ -0,0 +1,39 @@
+/* Seven Segment Clock Library
+ *  This library extends the Adafruit 7-seg LED library to function as a clock or timer. 
+ *  Written by Michael Coney
+ *  Created: 3/31/19
+ *  Updated: 7/30/23
+ * 
+ * 
+ */
+#ifndef SevenSegClock_h
+#define SevenSegClock_h
+
+#include "Arduino.h"
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+class SevenSegClock {
+  public:
+    SevenSegClock(int DISPLAY_ADDRESS);
+    void begin();
+    void timeConvert(int total_sec);
+    void error();
+    void blank();
+    void haywire(int duration);
+    void setSegDisplay(int minutes, int seconds);
+    void runTimer(int secondsToRun, int clockSpeed);
+
+  private:
+    Adafruit_7segment clockDisplay;
+    int _DISPLAY_ADDRESS;
+    bool colon = true;
+    int _currentMinutes;
+    int _currentSeconds;
+    int _setMinutes;
+    int _setSeconds;
+    int _totalSeconds;
+
+};
+
+#endif SevenSegClock_h
